44d558f633066cf4815c7d4c261a7bb3f25f0c53
Tony Ho <ho.tonym@gmail.com>
2018-05-13 15:19:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,166 @@
-# your code goes here
+def begins_with_r(array)
+
+  array.all? do |element|
+    element.start_with?("r")
+  end
+
+end
+
+def contain_a(array)
+  new_array = Array.new
+  array.select do |element|
+    if element.include?("a") == true
+      new_array << element
+    end
+  end
+  return new_array
+end
+
+def first_wa(array)
+    array.find do |element|
+    if element.instance_of?(String)
+      element.start_with?("wa")
+    end
+  end
+end
+
+def remove_non_strings(array)
+
+  array.select do |element|
+    element.instance_of?(String)
+  end
+
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+
+  array.map do |element|
+    counts[element] +=1
+  end
+
+counts
+end
+
+def count_elements(args)
+  names = []
+  count = []
+  args.each do |person|
+    duplicates = names.any? do |name|
+      name == person[:name]
+    end
+    if duplicates == true
+      position = names.index(person[:name])
+      count[position] += 1
+    else
+      names << person[:name]
+      count << 1 
+    end
+  end
+  result = []
+  i = 0 
+  while i < names.length 
+    result[i] = {
+      name: names[i],
+      count: count[i]
+    }
+    i += 1 
+  end 
+  result 
+  # binding.pry
+end
+
+# describe '#merge_data' do
+#   # Question 6
+#   it 'combines two nested data structures into one' do
+#     expect(merge_data(keys, data)).to eq(merged_data)
+#   end
+# end
+#
+# let(:merged_data) {
+#     [
+#           {
+#             :first_name => "blake",
+#             :awesomeness => 10,
+#                 :height => "74",
+#               :last_name => "johnson"
+#         },
+#           {
+#             :first_name => "ashley",
+#             :awesomeness => 9,
+#                 :height => 60,
+#               :last_name => "dubs"
+#         }
+#     ]
+#   }
+
+def merge_data(keys, data)
+  i = 0 
+  result = []
+  while i < keys.length
+    result[i] = keys[i].merge(data[0].values[i])
+    i += 1 
+  end 
+  result
+  # binding.pry
+end
+
+# describe '#find_cool' do
+#   # Question 7
+#   it 'find all cool hashes' do
+#     expect(find_cool(cool)).to eq([{:name => "blake",:temperature => "cool"}])
+#   end
+# end
+
+def find_cool(args)
+  result = []
+  args.each do |person|
+    if person[:temperature] == "cool"
+      result << person
+    end
+  end
+  result
+  # binding.pry
+end
+
+# describe '#organize_schools' do
+#   # Question 8
+#   it 'organizes the schools by location' do
+#     expect(organize_schools(schools)).to eq(organized_schools)
+#   end
+# end
+# let(:organized_schools) {
+#     {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+#     "SF"=>["dev boot camp", "Hack Reactor"],
+#     "Chicago"=>["dev boot camp chicago"]}
+# }
+
+
+def organize_schools(schools) 
+  new_hash = {}
+  locations = schools.values
+  names = schools.keys
+  city = []
+  locations.each do |location|
+    city << location[:location]
+  end
+  lookup = city.uniq
+  lookup.each do |place| 
+    group = []
+    i = 0 
+      while i < city.length
+        if city[i] == place
+          group << names[i]
+        end
+        i += 1 
+      end  
+      new_hash[place] = group
+  end
+  new_hash
+end
+
+
+
+
+
+
